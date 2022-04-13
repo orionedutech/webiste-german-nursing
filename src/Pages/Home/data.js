5f364642b088684ecfce3aedecf4d07dfdc99c0c
harsh-learnhat <harsh@learnhat.com>
2022-04-13 17:24:18 +0530

    meta tag code and gnw

--- src/Pages/Home/data.js
+++ src/Pages/Home/data.js
@@ -34,7 +34,7 @@ export const textdata = [
   "'This led to the digital transformation and growth of Orion to train and upskill them across core and soft skills. At Orion, we are charging towards forging the next generation of nation-builders for India.'",
 ];
 export const dataAboutNursingProogram = [
-  "Orion Edutech presents an incredible opportunity for nurses to build a nursing career in Germany. If you are a graduate of 4 year BSc / GNW / MSc, you are invited to apply for nursing jobs in Germany with great pay and benefits.",
+  "Orion Edutech presents an incredible opportunity for nurses to build a nursing career in Germany. If you are a graduate of 4 year BSc / GNM/ MSc, you are invited to apply for nursing jobs in Germany with great pay and benefits.",
 "Through our superior process and system, you will receive end to end support as a successful candidate right from training in German language and German culture to highly relevant immersion to make smooth transition for a great nursing career in Germany.",
 "If you are an eligible candidate and interested to a great career in Germany, you need to fill the form on this page and our counsellors will guide you to the process forward.Come and find incredible nursing jobs in Germany."
 ];
@@ -190,5 +190,5 @@ export const programFeesData = [
 ];
 
 // export const dataAboutNursingProogram = [
-//   "The Orion German Nursing Program is an incredible opportunity for 4 year BSc / GNW / MSc educated individuals looking to build a nursing career in Germany.",  "This program supports and trains eligible candidates in B1 and B2 Goethe Exams as well as trains them in German culture and other immersions for an easier career start in Germany.", "All successful candidates will be able to build successful nursing careers in Germany with top salary, benefits including social security, insurance and more.", "If you are interested candidate, fill the form on this micro site and our counsellors will get back to you with complete details. Come, build an incredible nursing career in Germany."
+//   "The Orion German Nursing Program is an incredible opportunity for 4 year BSc / GNM/ MSc educated individuals looking to build a nursing career in Germany.",  "This program supports and trains eligible candidates in B1 and B2 Goethe Exams as well as trains them in German culture and other immersions for an easier career start in Germany.", "All successful candidates will be able to build successful nursing careers in Germany with top salary, benefits including social security, insurance and more.", "If you are interested candidate, fill the form on this micro site and our counsellors will get back to you with complete details. Come, build an incredible nursing career in Germany."
 // ]
